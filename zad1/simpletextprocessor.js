cffaaf7ae5c193554b7aebc2f34b7d3464606a34
chujchuj123 <maksymilian0606@wp.pl>
2024-12-01 21:45:53 +0100

    third commit

--- zad1/simpletextprocessor.js
+++ zad1/simpletextprocessor.js
@@ -1,4 +1,24 @@
-const processText = (text, { uppercase = false, reverse = false, removeSpecialChars = false } = {}) =>{
-    const {uppercase, reverse, removeSpecialChars} = processText
-}
-processText('dubudusghsfjdjgsjg',uppercase=true,reverse=false,removeSpecialChars=true)
+const processText = (text, { uppercase = false, reverse = false, removeSpecialChars = false } = {}) => {
+    let processedText = [...text];
+    
+    if (removeSpecialChars) {
+        processedText = processedText.filter(char => /[a-zA-Z0-9\s]/.test(char));
+    }
+
+    if (reverse) {
+        processedText = processedText.reverse();
+    }
+
+    processedText = processedText.join('');
+
+    if (uppercase) {
+        processedText = processedText.toUpperCase();
+    }
+
+    return `Processed Text: ${processedText}`;
+};
+
+const text = "JakIs, takI tEkst!!!";
+const options = { uppercase: true, reverse: true, removeSpecialChars: true };
+console.log(processText(text, options));
+
